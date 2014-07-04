50d2f6c153275638cc1dce993546e401d779f38f
Henrique Bueno <hbuenos@gmail.com>
2014-07-04 16:34:35 +0200

    Updated grid_surfer cookbook with

--- cookbook/grid_surfer.py
+++ cookbook/grid_surfer.py
@@ -1,7 +1,7 @@
 """
 Gridding: Load a Surfer ASCII grid file
 """
-from fatiando import datasets, gridder
+from fatiando import datasets, gridder, gravmag
 from fatiando.vis import mpl
 
 # Fetching Bouguer anomaly model data (Surfer ASCII grid file)"
@@ -21,3 +21,26 @@ mpl.xlabel('y points to East (km)')
 mpl.ylabel('x points to North (km)')
 mpl.m2km()
 mpl.show()
+
+## Now do the upward continuation using the analytical formula
+height = 250000
+area = [min(x), max(x), min(y), max(y)]
+dims = gridder.spacing(area, shape)
+bouguercont = gravmag.transform.upcontinue(bouguer, height, x, y, dims)
+
+# Plotthe upward continuation
+mpl.figure()
+mpl.axis('scaled')
+mpl.title("Continued to %d km height" % ((height/1000)))
+mpl.contourf(y, x, bouguercont, shape, 15)
+cb = mpl.colorbar()
+cb.set_label('mGal')
+mpl.xlabel('y points to East (km)')
+mpl.ylabel('x points to North (km)')
+mpl.m2km()
+mpl.show()
+
+# Name of the output file
+fname = ('bouguer_alps_egm08_height_%d_km.grd' % ((height/1000)))
+# Saving the upward continued data in a Surfer ascii grid file
+gridder.dump_surfer(fname,bouguercont, shape, x, y, fmt='ascii')
