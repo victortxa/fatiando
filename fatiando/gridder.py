0701c856e3383f13e7f74ac34e3651310eb9de74
Leonardo Uieda <leouieda@gmail.com>
2014-10-07 12:03:56 -0300

    Copied the Grid class from the prototype repo

--- fatiando/gridder.py
+++ fatiando/gridder.py
@@ -1,6 +1,13 @@
 """
 Create and operate on grids and profiles.
 
+All the main functionality of this module (and more) is implemented in the
+:class:`~fatiando.gridder.Grid` class.
+Use it to generate grids of points, load from file, plot data, interpolate,
+and work with `Cartopy <http://scitools.org.uk/cartopy/>`__ projections.
+
+You can still use the functions below to achieve some of this functionality.
+
 **Grid generation**
 
 * :func:`~fatiando.gridder.regular`
@@ -33,6 +40,517 @@ Create and operate on grids and profiles.
 import numpy
 import scipy.interpolate
 import matplotlib.mlab
+from matplotlib import pyplot
+
+
+class Grid(object):
+    """
+    Grid data generation, manipulation, I/O and plotting.
+    """
+
+    _coord_names = ['x', 'y', 'lon', 'long', 'longitude', 'lat', 'latitude']
+    _special_attributes = ['shape', 'projection', 'lonlat', 'metadata']
+
+    def __init__(self, **kwargs):
+        kwargs = dict((k.lower(), v) for k, v in kwargs.iteritems())
+        added = []
+        for k, v in kwargs.iteritems():
+            if k not in self._special_attributes:
+                v = numpy.asarray(v).ravel()
+            if k in ['lon', 'long', 'longitude']:
+                k = 'lon'
+                if 'y' not in kwargs:
+                    setattr(self, 'y', v)
+                    added.append('y')
+            elif k in ['lat', 'latitude']:
+                k = 'lat'
+                if 'x' not in kwargs:
+                    setattr(self, 'x', v)
+                    added.append('x')
+            added.append(k)
+            setattr(self, k, v)
+        if 'shape' not in added:
+            self.shape = None
+        if 'projection' not in added:
+            self.projection = None
+        if 'x' not in kwargs or 'y' not in kwargs:
+            self.lonlat = True
+        else:
+            self.lonlat = False
+        not_attributes = set(self._coord_names + self._special_attributes)
+        self._attributes = list(set(added).difference(not_attributes))
+        # Check all attributes have the same size
+        sizes = [getattr(self, k).size for k in self._attributes + ['x', 'y']]
+        assert numpy.all(numpy.asarray(sizes) == sizes[0]), \
+            "All attributes and coordinates must have same size"
+        self.size = sizes[0]
+
+    def add_attribute(self, name, value):
+        """
+        Add a column to the grid.
+        """
+        value = numpy.asarray(value).ravel()
+        assert value.size == self.size, "Attribute must have same size as grid"
+        setattr(self, name.lower(), value)
+        self._attributes.append(name)
+        return self
+
+    @staticmethod
+    def regular(area, shape, z=None, lonlat=False, projection=None):
+        """
+        Generate a regular grid
+        """
+        x1, x2, y1, y2 = area
+        ny, nx = shape
+        x, y = numpy.meshgrid(numpy.linspace(x1, x2, nx),
+                              numpy.linspace(y1, y2, ny))
+        args = dict(shape=shape, projection=projection)
+        if lonlat:
+            args['lon'] = y
+            args['lat'] = x
+        else:
+            args['y'] = y
+            args['x'] = x
+        if z is not None:
+            args['z'] = z*numpy.ones(shape[0]*shape[1])
+        return Grid(**args)
+
+    @staticmethod
+    def scatter(area, n, z=None, lonlat=False, projection=None, seed=None):
+        """
+        Generate a random scatter of points.
+        """
+        coords = scatter(area, n, z, seed)
+        x, y = coords[0], coords[1]
+        args = dict(shape=None, projection=projection)
+        if len(coords) == 3:
+            args['z'] = coords[2]
+        if lonlat:
+            args['lon'] = y
+            args['lat'] = x
+        else:
+            args['y'] = y
+            args['x'] = x
+        return Grid(**args)
+
+    @staticmethod
+    def load(fname, **kwargs):
+        """
+        Load data from a file.
+
+        Will try to guess the file type by the extension.
+        If it's an open file, will pass it to
+        :meth:`~fatiando.gridder.Grid.load_numpy`.
+        """
+        grid = None
+        if isinstance(fname, str):
+            # Try to guess the file type from the extension
+            ext = fname.strip().split('.')[-1]
+            if ext == 'gdf':
+                grid = Grid.load_gdf(fname, **kwargs)
+            elif ext == 'csv':
+                grid = Grid.load_csv(fname, **kwargs)
+        if grid is None:
+            if 'binary' not in kwargs:
+                try:
+                    grid = Grid.load_numpy(fname, binary=False, **kwargs)
+                except:
+                    grid = Grid.load_numpy(fname, binary=True, **kwargs)
+            else:
+                grid = Grid.load_numpy(fname, **kwargs)
+        return grid
+
+    @staticmethod
+    def load_csv(fname, column_names=None, **kwargs):
+        """
+        Load data from a CSV file.
+        """
+        with open(fname) as f:
+            # Check if first line contains the column names
+            first = f.readline().strip().split(';')
+            columns = [s.strip() for s in first if s.strip()]
+            numbers = False
+            for item in columns:
+                try:
+                    float(item)
+                    numbers = True
+                except ValueError:
+                    pass
+            if numbers:
+                # Rewind the file to the origin to read later
+                f.seek(0)
+            else:
+                if column_names is None:
+                    column_names = columns
+            grid = Grid.load_numpy(f, column_names=column_names, binary=False,
+                                   delimiter=';')
+        return grid
+
+    @staticmethod
+    def load_numpy(fname, binary=False, column_names=None, **kwargs):
+        """
+        Load data from a numpy saved text or binary file.
+        """
+        if binary:
+            data = numpy.load(fname, **kwargs).T
+        else:
+            kwargs['unpack'] = True
+            data = numpy.loadtxt(fname, **kwargs)
+        if column_names is None:
+            nattr = len(data) - 2
+            attributes = ['data{:d}'.format(i + 1) for i in range(nattr)]
+            column_names = ['x', 'y'] + attributes
+        assert len(column_names) == len(data), "Insuffient column names."
+        args = dict([k, v] for k, v in zip(column_names, data))
+        return Grid(**args)
+
+    @staticmethod
+    def load_gdf(fname, **kwargs):
+        """
+        Load data from an ICGEM .gdf file.
+        """
+        with open(fname) as f:
+            # Read the header and extract metadata
+            metadata = []
+            shape = [None, None]
+            size = None
+            height = None
+            attributes = None
+            attr_line = False
+            for line in f:
+                if line.strip()[:11] == 'end_of_head':
+                    break
+                metadata.append(line)
+                if not line.strip():
+                    attr_line = True
+                    continue
+                if not attr_line:
+                    parts = line.strip().split()
+                    if parts[0] == 'height_over_ell':
+                        height = float(parts[1])
+                    elif parts[0] == 'latitude_parallels':
+                        shape[0] = int(parts[1])
+                    elif parts[0] == 'longitude_parallels':
+                        shape[1] = int(parts[1])
+                    elif parts[0] == 'number_of_gridpoints':
+                        size = int(parts[1])
+                else:
+                    attributes = line.strip().split()
+                    attr_line = False
+            # Read the numerical values
+            data = numpy.loadtxt(f, unpack=True)
+        # Sanity checks
+        if any(n is None for n in shape):
+            shape = None
+        else:
+            shape = tuple(shape)
+            if size is not None and shape[0]*shape[1] != size:
+                raise ValueError(
+                    "Grid shape {} and size read ({})".format(shape, size)
+                    + " are incompatible.")
+        if attributes is None:
+            raise ValueError("Couldn't read column names.")
+        if len(attributes) != len(data):
+            raise ValueError(
+                "Number of columns names ({})".format(len(attributes))
+                + " doesn't match columns read ({}).".format(len(data)))
+        # Make the argument list for the grid
+        args = dict(shape=shape, metadata=''.join(metadata))
+        if (height is not None) and ('height' not in attributes):
+            if shape is None:
+                if size is not None:
+                    n = size
+                else:
+                    n = data[0].size
+            else:
+                n = shape[0]*shape[1]
+            args['height'] = height*numpy.ones(n)
+        for attr, value in zip(attributes, data):
+            args[attr] = value
+        return Grid(**args)
+
+    def dump_csv(self, fname):
+        """
+        Save the data to a CSV file.
+        """
+        if isinstance(fname, str):
+            f = open(fname, 'w')
+        else:
+            f = fname
+        if self.lonlat:
+            column_names = ['latitude', 'longitude']
+        else:
+            column_names = ['x', 'y']
+        column_names.extend(self.get_attributes())
+        f.write('; '.join(column_names))
+        f.write('\n')
+        data = (getattr(self, i) for i in ['x', 'y'] + self.get_attributes())
+        numpy.savetxt(f, numpy.hstack(c.reshape((self.size, 1)) for c in data),
+                      delimiter='; ')
+        if isinstance(fname, str):
+            f.close()
+
+    @property
+    def area(self):
+        """
+        Return the [xmin, xmax, ymin, ymax] limits of the data
+        """
+        return (self.x.min(), self.x.max(), self.y.min(), self.y.max())
+
+    @property
+    def spacing(self):
+        """
+        If the grid is regular (has a ``shape`` attribute), this will be the
+        grid spacing.
+        """
+        if self.shape is None:
+            raise ValueError("Can't calculate spacing for irregular grids."
+                             + " Grid must have a shape attribute.")
+        x1, x2, y1, y2 = self.area
+        ny, nx = self.shape
+        dx = (x2 - x1)/(nx - 1)
+        dy = (y2 - y1)/(ny - 1)
+        return dx, dy
+
+    def get_attributes(self):
+        """
+        Return a list of the attributes (column names) of the grid
+        """
+        return list(self._attributes)
+
+    def interp_at(self, x, y, attribute=None, algorithm='cubic',
+                  extrapolate=False):
+        """
+        Interpolate the grid data on the x, y points given.
+        """
+        if attribute is None:
+            attribute = self._attributes
+        elif isinstance(attribute, str):
+            attribute = [attribute]
+        args = dict(x=x, y=y, projection=self.projection)
+        for k in attribute:
+            args[k] = interp_at(self.x, self.y, getattr(self, k), x, y,
+                                algorithm, extrapolate)
+        return self.__class__(**args)
+
+    def interp(self, shape, attribute=None, area=None, algorithm='cubic',
+               extrapolate=False):
+        """
+        Interpolate the grid data on a regular grid.
+        """
+        ny, nx = shape
+        if area is None:
+            area = self.area
+        x1, x2, y1, y2 = area
+        xs = numpy.linspace(x1, x2, nx)
+        ys = numpy.linspace(y1, y2, ny)
+        xp, yp = [i.ravel() for i in numpy.meshgrid(xs, ys)]
+        if attribute is None:
+            attribute = self._attributes
+        elif isinstance(attribute, str):
+            attribute = [attribute]
+        args = dict(x=xp, y=yp, shape=shape, projection=self.projection)
+        for k in attribute:
+            args[k] = interp_at(self.x, self.y, getattr(self, k), xp, yp,
+                                algorithm, extrapolate)
+        return self.__class__(**args)
+
+    def profile(self, point1, point2, npoints, attribute=None,
+                extrapolate=False):
+        """
+        Extract a profile between point 1 and point 2.
+        """
+        if attribute is None:
+            attribute = self._attributes
+        elif isinstance(attribute, str):
+            attribute = [attribute]
+        args = dict(shape=None, projection=self.projection)
+        for k in attribute:
+            x, y, dist, args[k] = profile(self.x, self.y, getattr(self, k),
+                                          point1, point2, npoints,
+                                          extrapolate=extrapolate)
+        return self.__class__(x=x, y=y, distance=dist, **args)
+
+    def set_projection(self, proj):
+        """
+        Specify the Cartopy projection object that corresponds to the
+        projection of the data.
+        """
+        self.projection = proj
+        return self
+
+    def reproject(self, target_proj):
+        """
+        Transform the coordinates from the current projection to the given
+        projection.
+        """
+        if self.projection is None:
+            raise AttributeError(
+                'Call .set_projection to specify the grid projection first.')
+        coords = target_proj.transform_points(self.projection, self.y, self.x)
+        y, x = coords.T[:2]
+        args = dict((k, getattr(self, k)) for k in self._attributes)
+        grid = self.__class__(x=x, y=y, projection=target_proj,
+                              shape=self.shape, **args)
+        return grid
+
+    def points(self, style='.k', ax=None, **kwargs):
+        """
+        Plot the data points on a map.
+        """
+        if ax is None:
+            if self.projection is None:
+                ax = pyplot.gca()
+            else:
+                ax = pyplot.axes(projection=self.projection)
+        if self.projection is not None:
+            kwargs['transform'] = self.projection
+        plot = ax.plot(self.y, self.x, style, **kwargs)
+        return plot
+
+    def pcolor(self, attribute, autoranges=True, colorbar='vertical', ax=None,
+               basemap=None, **kwargs):
+        """
+        Make a pseudo-color map of the given attribute.
+        """
+        if isinstance(attribute, str):
+            attribute = getattr(self, attribute)
+        values = numpy.ma.masked_where(
+            numpy.isnan(attribute), attribute).reshape(self.shape)
+        _autocmap(values, autoranges, kwargs)
+        if ax is None:
+            if self.projection is None:
+                ax = pyplot.gca()
+            else:
+                ax = pyplot.axes(projection=self.projection)
+        X, Y = self.y.reshape(self.shape), self.x.reshape(self.shape)
+        if basemap is not None:
+            X, Y = basemap(X, Y)
+            plot = basemap.pcolormesh(X, Y, values, **kwargs)
+        else:
+            if 'transform' not in kwargs and self.projection is not None:
+                kwargs['transform'] = self.projection
+            plot = ax.pcolormesh(X, Y, values, **kwargs)
+        if colorbar is not None:
+            _add_colorbar(plot, colorbar, ax)
+        x1, x2, y1, y2 = self.area
+        ax.set_xlim(y1, y2)
+        ax.set_ylim(x1, x2)
+        return plot
+
+    def contourf(self, attribute, levels=10, autoranges=True,
+                 colorbar='vertical', ax=None, basemap=None, **kwargs):
+        """
+        Make a filled contour map of the given attribute.
+        """
+        if isinstance(attribute, str):
+            attribute = getattr(self, attribute)
+        values = numpy.ma.masked_where(
+            numpy.isnan(attribute), attribute).reshape(self.shape)
+        _autocmap(values, autoranges, kwargs)
+        if ax is None:
+            if self.projection is None:
+                ax = pyplot.gca()
+            else:
+                ax = pyplot.axes(projection=self.projection)
+        X, Y = self.y.reshape(self.shape), self.x.reshape(self.shape)
+        if basemap is not None:
+            X, Y = basemap(X, Y)
+            plot = basemap.contourf(X, Y, values, levels, **kwargs)
+        else:
+            if 'transform' not in kwargs and self.projection is not None:
+                kwargs['transform'] = self.projection
+            plot = ax.contourf(X, Y, values, levels, **kwargs)
+        if colorbar is not None:
+            _add_colorbar(plot, colorbar, ax)
+        x1, x2, y1, y2 = self.area
+        ax.set_xlim(y1, y2)
+        ax.set_ylim(x1, x2)
+        return plot
+
+    def contour(self, attribute, levels=10, autoranges=True, ax=None,
+                basemap=None, label=None, clabel=True, style='solid',
+                linewidth=1.0, **kwargs):
+        """
+        Make a contour plot of the given attribute
+        """
+        if isinstance(attribute, str):
+            attribute = getattr(self, attribute)
+        values = numpy.ma.masked_where(
+            numpy.isnan(attribute), attribute).reshape(self.shape)
+        if 'colors' not in kwargs:
+            _autocmap(values, autoranges, kwargs)
+        if ax is None:
+            if self.projection is None:
+                ax = pyplot.gca()
+            else:
+                ax = pyplot.axes(projection=self.projection)
+        X, Y = self.y.reshape(self.shape), self.x.reshape(self.shape)
+        if basemap is not None:
+            X, Y = basemap(X, Y)
+            ct = basemap.contour(X, Y, values, levels, **kwargs)
+        else:
+            if 'transform' not in kwargs and self.projection is not None:
+                kwargs['transform'] = self.projection
+            ct = ax.contour(X, Y, values, levels, **kwargs)
+        if clabel:
+            ct.clabel(fmt='%g')
+        if label is not None:
+            ct.collections[0].set_label(label)
+        if style != 'mixed':
+            for c in ct.collections:
+                c.set_linestyle(style)
+        for c in ct.collections:
+            c.set_linewidth(linewidth)
+        x1, x2, y1, y2 = self.area
+        ax.set_xlim(y1, y2)
+        ax.set_ylim(x1, x2)
+        return ct
+
+
+def _autocmap(values, autoranges, args):
+    """
+    Automatically choose a color map for the given data.
+    Also adjust the value range for diverging colormaps so that 0 is the middle
+    color.
+    """
+    vmax, vmin = numpy.nanmax(values), numpy.nanmin(values)
+    if vmax <= 0 or vmin >= 0:
+        diverging = False
+        if vmax <= 0:
+            sign = 'negative'
+        else:
+            sign = 'positive'
+    else:
+        diverging = True
+    has_vmin_vmax = any(k in args for k in ['vmin', 'vmax'])
+    if autoranges and diverging and not has_vmin_vmax:
+        absmax = numpy.abs([vmax, vmin]).max()
+        args['vmin'] = -absmax
+        args['vmax'] = absmax
+    if 'cmap' not in args:
+        if diverging:
+            args['cmap'] = pyplot.cm.RdBu_r
+        else:
+            if sign == 'positive':
+                args['cmap'] = pyplot.cm.Reds
+            elif sign == 'negative':
+                args['cmap'] = pyplot.cm.Blues_r
+
+
+def _add_colorbar(mappable, orientation, ax):
+    """
+    Add a color bar to the given mappable object (returned by a matplotlib
+    plot command).
+    """
+    cbargs = dict(orientation=orientation)
+    if orientation == 'horizontal':
+        cbargs['pad'] = 0.02
+        cbargs['aspect'] = 50
+    else:
+        cbargs['pad'] = 0
+        cbargs['aspect'] = 20
+    ax.get_figure().colorbar(mappable, ax=ax, **cbargs)
 
 
 def load_surfer(fname, fmt='ascii'):
