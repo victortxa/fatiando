dc0db62f9af8352f1b83a780eeb49109c2e1c7a5
victortxa <victortxa@gmail.com>
2016-09-20 17:27:19 -0300

    Adjusted cookbook file seismic_srtomo_sharp.py

--- cookbook/seismic_srtomo_sharp.py
+++ cookbook/seismic_srtomo_sharp.py
@@ -7,7 +7,7 @@ from fatiando.mesher import SquareMesh
 from fatiando.seismic import ttime2d, srtomo
 from fatiando.inversion.regularization import TotalVariation2D
 from fatiando.vis import mpl
-from fatiando import utils
+from fatiando import utils, gridder
 
 area = (0, 500000, 0, 500000)
 shape = (30, 30)
@@ -18,9 +18,9 @@ model.addprop('vp', vel.ravel())
 
 # Make some travel time data and add noise
 seed = 0  # Set the random seed so that points are the same everythime
-src_loc = utils.random_points(area, 80, seed=seed)
-rec_loc = utils.circular_points(area, 30, random=True, seed=seed)
-srcs, recs = utils.connect_points(src_loc, rec_loc)
+src_loc = gridder.random_points(area, 80, seed=seed)
+rec_loc = gridder.circular_points(area, 30, random=True, seed=seed)
+srcs, recs = gridder.connect_points(src_loc, rec_loc)
 tts = ttime2d.straight(model, 'vp', srcs, recs)
 tts, error = utils.contaminate(tts, 0.02, percent=True, return_stddev=True,
                                seed=seed)
